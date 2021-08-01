6516013b96086ef2915590e73b22507588495cf2
Eugene Orlov <jaskon97@gmail.com>
2021-08-01 04:12:29 +0300

    Guessing game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,23 @@
 class GuessingGame {
-    constructor() {}
-
     setRange(min, max) {
+        this.min = min;
+        this.max = max;
+    }
 
+    getMiddle() {
+        return Math.round((this.max - this.min) / 2) + this.min;
     }
 
     guess() {
-
+        return this.getMiddle();
     }
 
     lower() {
-
+        this.max = this.getMiddle();
     }
 
     greater() {
-
+        this.min = this.getMiddle();
     }
 }
 
